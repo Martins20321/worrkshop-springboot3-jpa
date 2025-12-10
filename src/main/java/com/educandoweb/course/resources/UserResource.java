444acd0a88932c241b8647395d0d8d4fef546410
Martins20321 <jgmsilva11@gmail.com>
2025-12-10 17:05:00 -0300

    User Delete - 204

--- src/main/java/com/educandoweb/course/resources/UserResource.java
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -38,10 +39,16 @@ public class UserResource {
 	}
 	
 	@PostMapping
-	public ResponseEntity<User> insert(@RequestBody User obj){
+	public ResponseEntity<User> insert(@RequestBody User obj){ //Para o Json conseguir desserializar e transformar em obj
 		obj = service.insert(obj);
 		//Utiizando o container Servlet do Tomcat
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
-		return ResponseEntity.created(uri).body(obj);
+		return ResponseEntity.created(uri).body(obj); //Boa prática = Buscar o acerto 201 created
+	}
+	
+	@DeleteMapping("/{id}") //Código 204 (HTTP)
+	public ResponseEntity<Void> delete(@PathVariable Long id){
+		service.Delete(id);
+		return ResponseEntity.noContent().build();
 	}
 }
