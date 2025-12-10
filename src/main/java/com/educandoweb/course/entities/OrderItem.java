a3a6f45f7f87c870112b305a104b49fd142c19e4
Martins20321 <jgmsilva11@gmail.com>
2025-12-10 10:38:06 -0300

    OrderItem, many-to-many association with extra attributes - Part 2

--- src/main/java/com/educandoweb/course/entities/OrderItem.java
+++ src/main/java/com/educandoweb/course/entities/OrderItem.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 import java.util.Objects;
 
 import com.educandoweb.course.entities.pk.OrderItemPK;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
@@ -16,7 +17,7 @@ public class OrderItem implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@EmbeddedId
-	private OrderItemPK id; //O id acessa a classe OrderItemPK
+	private OrderItemPK id = new OrderItemPK(); //O id acessa a classe OrderItemPK
 	private Integer quantity;
 	private Double price;
 
@@ -32,6 +33,7 @@ public class OrderItem implements Serializable {
 		this.price = price;
 	}
 
+	@JsonIgnore //O java EE, estabelece que o padrão é o Get
 	public Order getOrder() {
 		return id.getOrder();
 	}
